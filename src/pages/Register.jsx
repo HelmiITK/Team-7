f27cd92782344caa4064f5ad9300dc6700719ceb
srdevina <142998071+srdevina@users.noreply.github.com>
2023-10-18 18:45:30 +0700

    Add files via upload

---
+++ src/pages/Register.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/v1/auth/register`,
+        {
+          email,
+          name,
+          password,
+        }
+      );
+
+      if (response.status === 201) {
+        // You can handle registration success here, e.g., show a success message.
+        alert("Registration successful!");
+      } else {
+        alert("Registration failed. Please try again.");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error?.response?.data?.message);
+      } else {
+        alert(error?.message);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={register}>
+      <h1 className="align-center">Register</h1>
+      <div className="mb-3">
+        <label>Name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Already registered <a href="/login">login ?</a>
+      </p>
+    </form>
+  );
+};
+
+export default Register;
